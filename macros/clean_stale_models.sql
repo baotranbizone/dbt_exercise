b1945e3439dd67b37c1bdff0ca383346d6b0752a
Bao Tran Quoc <bao.tran.quoc@bizone.com.vn>
2025-08-06 08:18:17 +0000

    advanced_jinja

---
+++ macros/clean_stale_models.sql
@@ -0,0 +1,37 @@
+{#
+    1. Query the info schema of a database
+    2. Find objects that are > 1 week old
+    3. Generate automated drop statements
+    4. Has the ability to execute those drop statements
+#}
+
+
+{% macro clean_stale_models(database=target.database, schema=target.schema, days=7, dry_run=True) %}
+
+    {% set get_drop_commands_query %}
+        select 
+            case
+                when table_type = 'VIEW'
+                    then table_type
+                else 
+                    'TABLE'
+            end as drop_type,
+            'DROP ' || drop_type || ' {{ database | upper }}.' || table_schema || '.' || table_name || ';'
+        from {{ database }}.information_schema.tables
+        where table_schema = '{{ schema }}'
+        and last_altered <= current_date - {{ days }}
+    {% endset %}
+
+    {{ log('\nGenerating cleanup queries...\n', info=True)}}
+    {% set drop_queries = run_query(get_drop_commands_query).columns[1].values() %}
+
+    {% for query in drop_queries %}
+        {% if dry_run %}
+            {{ log(query, info=True) }}             
+        {% else %}
+            {{ log('Dropping object with command: ' ~ query, info=True)}}
+            {{ do run_query(query)}}
+        {% endif %}
+    {% endfor %}
+
+{% endmacro %}
---
+++ models/union_tables_by_jaffle_shop_prefix.sql
@@ -0,0 +1,6 @@
+{{ 
+    union_tables_by_prefix(
+    database='lakehouse_bao',
+    schema='dbt_btranquoc',
+    prefix='jaffle_shop'
+)}}
